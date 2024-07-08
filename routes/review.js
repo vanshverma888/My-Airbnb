f492e35ef862a0c022581d25dafa7198411e53af
Vansh Verma <121630852+vanshverma888@users.noreply.github.com>
2024-07-08 12:59:40 +0530

    Add files via upload

---
+++ routes/review.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const wrapAsync = require("../utils/wrapAsync.js");
+const Reviews = require("../models/review.js");
+const Listing = require("../models/listing.js");
+const {validateReview, isLoggedIn , isReviewAuthor} = require("../middleware.js");
+
+const reviewController = require("../controller/review.js");
+
+
+// Review 
+// post review route
+router.post("/", isLoggedIn ,validateReview , wrapAsync(reviewController.createReview));
+
+// delete review route
+router.delete("/:reviewId", isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
+
+module.exports = router;
